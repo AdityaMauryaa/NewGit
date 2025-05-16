959990a50c3593dc541aa233168d4e12dfcb3343
Aditya Maurya <adityamauryanitt@gmail.com>
2025-05-16 21:14:11 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,9 @@
+function something() {
+  console.log("This is a test");
+}
+function anotherThing() {
+  console.log("This is another test");
+}
+function yetAnotherThing() {    
+    console.log("This is yet another test");
+    }           
